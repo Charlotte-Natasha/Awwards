48f4876ac8c6da480256f33b55b10c1e0796906e
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-12 21:05:06 +0200

    edited models.py

--- wardapp/models.py
+++ wardapp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     profile_picture = models.ImageField(null=True, blank=True, upload_to='profilepics')
     bio = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

7a7adb8b8919365372af7d3493002526ae3bbb81
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-13 06:13:42 +0200

    added review models

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -140,6 +140,6 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL ='login'
+# LOGIN_URL ='login'
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = '/login'

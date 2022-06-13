9db468850c137ee511b7a7c5c2fff01b42ef4846
Charlotte Natasha <natashaonyango97@gmail.com>
2022-06-13 15:43:23 +0200

    changed rate choices in models

--- wardapp/models.py
+++ wardapp/models.py
@@ -43,16 +43,12 @@ class Project(models.Model):
         return cls.objects.filter(title__icontains=name).all()
 
 RATE_CHOICES = [
-(1,'1- Trash'),
-(2,'2- Horrible'),
-(3,'3- Terrible'),
-(4,'4- Bad'),
-(5,'5- Ok'),
-(6,'6- Watchable'),
-(7,'7- Good'),
-(8,'8- Very Good'),
-(9,'9- perfect'),
-(10,'10- Master Piece'),
+(1,'1-Ok'),
+(2,'2-Watchable'),
+(3,'3-Good'),
+(4,'4-Very Good'),
+(5,'5-Perfect'),
+(6,'6-Master Piece'),
 ]
 
 class Review(models.Model):
